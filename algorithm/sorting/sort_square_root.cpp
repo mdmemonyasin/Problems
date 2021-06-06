de0e7df15091fafafe771d990e23bcca97f24f6c
mdmemonyasin <memonyasin3@gmail.com>
2021-06-06 15:35:46 +0530

    added some problems

---
+++ algorithm/sorting/sort_square_root.cpp
@@ -0,0 +1,44 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n, x;
+    cin>>n;
+    vector<int> arr;
+    vector<int> ans;
+    for(int i=0;i<n;i++){
+        cin>>x;
+        arr.push_back(x);
+    }
+    int i = 0, j = 0, c = 0;
+    for(;i<n;i++){
+        if(arr[i] >= 0){
+            break;
+        }
+    }
+    if(i>0){
+        j = i-1;
+    }
+    cout<<i<<" "<<j<<endl;
+    for(;i<arr.size()&&j>=0;){
+        if(arr[i] * arr[i] < arr[j] * arr[j]){
+            ans.push_back(arr[i] * arr[i]);
+            i++;
+        }else{
+            ans.push_back(arr[j] * arr[j]);
+            j--;
+        }
+    }
+    while(i<n){
+        ans.push_back(arr[i] * arr[i]);
+        i++;
+    }
+    while(j>-1){
+        ans.push_back(arr[j] * arr[j]);
+        j--;
+    }
+    for(int i=0;i<n;i++){
+        cout<<ans[i]<<" ";
+    }
+    cout<<endl;
+}
